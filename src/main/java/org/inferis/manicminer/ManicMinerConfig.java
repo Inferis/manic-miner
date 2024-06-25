1fdb1415d28d6b0e74134087f74cee91df6d40f5
Tom <tom@inferis.org>
2024-06-25 10:06:08 +0200

    Use Json to store config

--- src/main/java/org/inferis/manicminer/ManicMinerConfig.java
+++ src/main/java/org/inferis/manicminer/ManicMinerConfig.java
@@ -1,17 +1,20 @@
 package org.inferis.manicminer;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
-import com.mojang.datafixers.types.Type.FieldNotFoundException;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 
 import net.fabricmc.loader.api.FabricLoader;
 
 public class ManicMinerConfig {
+	private static final String CONFIG_FILE = FabricLoader.getInstance().getConfigDir().resolve("manicminer.cfg").toString();
+
     public boolean mustSneak = true;
     public boolean allowCommonBlocks = true;
     public boolean removeCommonBlocksAroundOre = true;
@@ -20,39 +23,42 @@ public class ManicMinerConfig {
     public int maxWoodSize = 192;
 
     public void save() {
-        var filename = FabricLoader.getInstance().getConfigDir().resolve("manicminer.cfg");
-        var file = new File(filename.toString());
+        var file = new File(CONFIG_FILE);
         try {
-            var writer = new FileWriter(file);
-            writer.write((mustSneak ? "true" : "false") + " # mustSneak\n");
-            writer.write((allowCommonBlocks ? "true" : "false") + " # allowCommonBlocks\n");
-            writer.write((removeCommonBlocksAroundOre ? "true" : "false") + " # removeCommonBlocksAroundOre\n");
-            writer.write(maxVeinSize + " # maxVeinSize\n");
-            writer.write(maxCommonSize + " # maxCommonSize\n");
-            writer.write(maxWoodSize + " # maxWoodSize\n");
-            writer.close();
+            var writer = Files.asCharSink(file, Charsets.UTF_8);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            writer.write(gson.toJson(this));
         } catch (IOException e) {
             ManicMiner.LOGGER.error("Could not save config", e);
         }
     }
 
     public void load() {
-        var filename = FabricLoader.getInstance().getConfigDir().resolve("manicminer.cfg");
-        var file = new File(filename.toString());
-        if (file.exists()) {
-            try {
-                var scanner = new Scanner(file);
-                mustSneak = scanner.nextBoolean(); scanner.nextLine();
-                allowCommonBlocks = scanner.nextBoolean(); scanner.nextLine();
-                removeCommonBlocksAroundOre = scanner.nextBoolean(); scanner.nextLine();
-                maxVeinSize = scanner.nextInt(); scanner.nextLine();
-                maxCommonSize = scanner.nextInt(); scanner.nextLine();
-                maxWoodSize = scanner.nextInt(); scanner.nextLine();
-                scanner.close();
-            }
-            catch (FileNotFoundException e) {
-                ManicMiner.LOGGER.error("Could not open config", e);
-            }
+        var file = new File(CONFIG_FILE);
+        if (!file.exists()) {
+            return;
+        }
+
+        try {
+            var json = Files.asCharSource(file, Charsets.UTF_8).read();
+            Gson gson = new Gson();
+            var loadedConfig = gson.fromJson(json, ManicMinerConfig.class);
+
+            mustSneak = loadedConfig.mustSneak;
+            allowCommonBlocks = loadedConfig.allowCommonBlocks;
+            removeCommonBlocksAroundOre = loadedConfig.removeCommonBlocksAroundOre;
+            maxVeinSize = loadedConfig.maxVeinSize;
+            maxCommonSize = loadedConfig.maxCommonSize;
+            maxWoodSize = loadedConfig.maxWoodSize;
+        }
+        catch (FileNotFoundException e) {
+            ManicMiner.LOGGER.error("Could not open config", e);
+        }
+        catch (IOException e) {
+            ManicMiner.LOGGER.error("Could not read config", e);
+        }
+        catch (JsonSyntaxException e) {
+            ManicMiner.LOGGER.error("Could not read config, syntax error", e);
         }
     }
 }
